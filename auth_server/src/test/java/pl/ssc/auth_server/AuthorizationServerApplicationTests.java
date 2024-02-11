cb389fa73ff66dd1092c13b2d3d8f42760fa1d94
zukowskimichal <michalzukowski93@gmail.com>
2024-02-11 12:45:33 +0100

    initial

---
+++ auth_server/src/test/java/pl/ssc/auth_server/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ssc.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

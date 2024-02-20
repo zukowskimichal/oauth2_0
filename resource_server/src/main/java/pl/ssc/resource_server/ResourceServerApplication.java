7ee39364b49a71e328cb405ccf25f00eab6ee2c8
zukowskimichal <michalzukowski93@gmail.com>
2024-02-20 16:16:24 +0100

    resource server

---
+++ resource_server/src/main/java/pl/ssc/resource_server/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package pl.ssc.resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+}
